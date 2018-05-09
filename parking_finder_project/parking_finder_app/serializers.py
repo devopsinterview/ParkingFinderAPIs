538322b647ce357c2a50769d0dce61232266a7d3
devopsinterview <visumrs@yahoo.com>
2018-05-08 19:56:06 -0700

    V2 - added pricing_details to parking serializer

--- parking_finder_project/parking_finder_app/serializers.py
+++ parking_finder_project/parking_finder_app/serializers.py
@@ -5,7 +5,11 @@ from .models import ParkingSpace
 
 class ParkingSpaceSerializer(serializers.ModelSerializer):
     booked_till = serializers.DateTimeField()
+    pricing_details = serializers.SerializerMethodField()
 
     class Meta:
         model = ParkingSpace
         fields = "__all__"
+
+    def get_pricing_details(self, obj):
+        return {'pricing_per_hour': '$3.00'}
